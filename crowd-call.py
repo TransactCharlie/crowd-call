c1884278bde0e32ca39436071e97a8d85d0b8498
Charlie Gildawie <charlie.gildawie@skyscanner.net>
2016-12-13 10:50:06 +0000

    cookies

--- crowd-call.py
+++ crowd-call.py
@@ -16,28 +16,43 @@ def echo_out(sess, resp):
     machine_id = j["debug"]["machine_id"]
     log.info(machine_id)
     MACHINES[machine_id] +=1
-    log.info(resp.cookies)
+    for c in resp.cookies:
+        COOKIES[c.value] +=1
 
 @click.command()
 @click.option('-u', '--url', help='http(s) request to make', prompt=True)
 @click.option('-t', '--tries', default=1, help='number of tries to make')
 @click.option('-c', '--concurrency', default=1, help='num of concurrent reqs' )
 def make_requests(url, tries, concurrency):
+
+    # Cookie
+    cookies = {"X-Mapping-rrsqbjcb": "06a13f0a788e803fcc56e78802891a26"}
+
+
     log.info("=== Starting Run with params ================================")
     log.info("url: {}".format(url))
     log.info("tries: {}".format(tries))
     log.info("concurrency: {}".format(concurrency))
+    log.info("cookies: {}".format(cookies))
     log.info("=============================================================")
 
     # Create  a request session
     session = FuturesSession(max_workers=concurrency)
 
     # Generate request tasks
-    tasks = [session.get(url, background_callback=echo_out) for i in range(0, tries)]
+    tasks = [
+        session.get(
+            url,
+            cookies=cookies,
+            background_callback=echo_out
+            )
+        for i in range(0, tries)
+        ]
 
     # Wait for all tasks to finish
     responses = [r.result() for r in tasks]
     print(dict(MACHINES))
+    print(dict(COOKIES))
 
 if __name__ == "__main__":
     make_requests()
